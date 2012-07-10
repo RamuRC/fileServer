39a178f1c00bbbd85e5080707ef3a699b6264b72
xizhu <xizhu.wht@taobao.com>
2012-07-10 19:23:50 +0800

    添加直接返回一个文件列表的功能

--- requestHandler.js
+++ requestHandler.js
@@ -3,7 +3,7 @@ var exec = require("child_process").exec;
 
 function readFile(pathName, response) {
 	console.log(pathName);
-
+	
 	fs.readFile("."+pathName,function(err,data) {
 		if(!err) {
 			console.log(data);
@@ -14,6 +14,18 @@ function readFile(pathName, response) {
 		}
 	});	
 	
+	fs.readdir("."+pathName, function(err,files) {
+		if(!err) {
+			console.log("Request for " + pathName + " received.");
+			
+			console.log(files);
+			
+			var content = HTMLFileContentWithDir(pathName, files);
+		    response.write(content);
+		    response.end();
+		    }
+		}); 	
+		
 	exec("ls -lah", function (error, stdout, stderr) {
 		response.writeHead(200, {"Content-Type": "text/plain"});
 	    response.write(stdout);
@@ -30,6 +42,21 @@ function start() {
   });
 
   return content;
+}
+
+function HTMLFileContentWithDir (pathName, files) {
+	var title = "<html>";
+	var taile = "</html>"
+	console.log("pathname" + pathName + "content :")
+	if(pathName.length > 1) {
+		pathName += "/";
+	}
+	for (i = 0; i < files.length; ++i) {
+		var str = "<a href = \"" + pathName + files[i] + "\">" + files[i] + "</a><br>";
+		console.log(str);
+		title = title + str;
+	}
+	return title + taile;
 }
 
 exports.readFile = readFile;
